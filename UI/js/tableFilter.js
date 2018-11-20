2e3d89d446a6c9180232a107976ae817510686d9
Damola <salisu.damola@yahoo.com>
2018-11-20 23:54:33 +0100

    Create a User Interface for listing records
    create the user my record page and add script for table filtering
    
    [Finishes #162080897]

---
+++ UI/js/tableFilter.js
@@ -0,0 +1,24 @@
+const selectField = document.getElementById('dashboard-table-select');
+
+selectField.addEventListener('change', () => {
+    let optionSelected = selectField.options[selectField.selectedIndex].value;
+    let allRows = document.querySelectorAll('tr.dashboard-table-row');
+
+    if(optionSelected !== "All"){
+
+        allRows.forEach( row => {
+            row.style.display = 'none';
+        })
+        document.querySelectorAll(`tr.${optionSelected}`).forEach( row => {
+            row.style.display = 'table-row';
+        })
+
+    }
+
+    else{
+        allRows.forEach( row => {
+            row.style.display = 'table-row';
+        })
+    }
+
+})
