2f977ac8b1a626ca239853323c8d529f0fed07a7
Damola <salisu.damola@yahoo.com>
2019-01-08 20:09:48 +0100

    chore(server.js): removed unneeded logic

--- server/server.js
+++ server/server.js
@@ -20,9 +20,6 @@ app.use(`${currApiPrefix}/img`, express.static(path.resolve('uploads', 'img')));
 app.use(`${currApiPrefix}/auth`, authRouter);
 app.use(`${currApiPrefix}/red-flags`, redFlagRouter);
 app.use(`${currApiPrefix}/interventions`, interventionRouter);
-app.get(`${currApiPrefix}/uploads/:filepath`, (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'uploads', req.params.filepath));
-});
 
 app.use('/', (req, res) => res.json({
   status: 404,
